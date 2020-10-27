acaeed6e95ad47910587cb8b9a575c25b90fc970
Dov <dovbenzak2@hotmail.com>
2020-10-27 10:43:28 +0100

    [FIX] Touch Movement (without orient)

--- Origami_PltProject_Team1/Assets/Scripts/DovScripts/TopDownController.cs
+++ Origami_PltProject_Team1/Assets/Scripts/DovScripts/TopDownController.cs
@@ -15,12 +15,27 @@ public class TopDownController : MonoBehaviour
         entity.Move(moveDir);
 
 
-        //if (Input.GetMouseButton(0))
-        //{
-        //    //Raycast --> ScreenPointToRay --> Input.mousePosition.x, 0f, Input.mousePosition.z
-        //    Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //    entity.MoveToDestination(worldMousePosition);
-        //}
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector3 touchPos = new Vector3(touch.position.x, touch.position.y, 0);
+            Ray ray = Camera.main.ScreenPointToRay(touchPos);
+
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green);
+                entity.MoveToDestination(hit.point);
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
+                Debug.Log("Did not Hit");
+            }
+
+        }
         //else
         //{
         //    entity.MoveStop();
