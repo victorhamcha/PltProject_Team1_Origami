6e487eac82e206bf8460da60f69f96bc02481d35
Vincent <vincent_1999@hotmail.fr>
2020-10-28 10:29:39 +0100

    [FIX] Update switch mode player -> origami

--- Origami_PltProject_Team1/Assets/Scripts/VincentScripts/testSwitchModePlayer.cs
+++ Origami_PltProject_Team1/Assets/Scripts/VincentScripts/testSwitchModePlayer.cs
@@ -2,20 +2,39 @@
 
 public class testSwitchModePlayer : MonoBehaviour
 {
-    [SerializeField] private GameObject pliageBoat;
-    [SerializeField] private Transform posPliage;
+    [SerializeField] private GameObject pliageBoat = null;
+    [SerializeField] private Transform posPliage = null;
 
     [SerializeField] private bool _activeModeOrigami = false;
-
     private bool _onModeOrigami = false;
 
-    // Update is called once per frame
+    private TopDownEntity movementPlayer;
+    private float _acceleration = 0f;
+    private float _speedMax = 0f;
+
+    void Start()
+    {
+        movementPlayer = GetComponent<TopDownEntity>();
+        _acceleration = movementPlayer.acceleration;
+        _speedMax = movementPlayer.speedMax;
+    }
+
     void Update()
     {
         if (_activeModeOrigami)
         {
             _activeModeOrigami = false;
             _onModeOrigami = !_onModeOrigami;
+            if (_onModeOrigami)
+            {
+                movementPlayer.acceleration = 0f;
+                movementPlayer.speedMax = 0f;
+            }
+            else
+            {
+                movementPlayer.acceleration = _acceleration;
+                movementPlayer.speedMax = _speedMax;
+            }
             pliageBoat.SetActive(_onModeOrigami);
             pliageBoat.transform.position = posPliage.position;
             pliageBoat.transform.rotation = posPliage.rotation;
