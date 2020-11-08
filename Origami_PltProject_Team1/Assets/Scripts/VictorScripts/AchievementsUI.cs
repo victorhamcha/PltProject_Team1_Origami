76208fe74c7001315ec149168461f7088f75b72d
Hamcha <victor.hamcha@gmail.com>
2020-11-08 22:34:39 +0100

    UI du menu fini

---
+++ Origami_PltProject_Team1/Assets/Scripts/VictorScripts/AchievementsUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementsUI : MonoBehaviour
+{
+   [SerializeField] private float offset;
+   [SerializeField] private int lockSucces;
+   [SerializeField] private int unlockSucces;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Origami_PltProject_Team1/Assets/Scripts/VictorScripts/Swiping.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swiping : MonoBehaviour
+{
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown = false;
+    private bool isDraging = false;
+    private Vector2 startTouch, swipeDelta;
+    public float pointMort = 200;
+
+    private void Update()
+    {
+        tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+        #region Standalone Inputs
+        if (Input.GetMouseButtonDown(0))
+        {
+            tap = true;
+            isDraging = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDraging = false;
+            Reset();
+        }
+
+        #endregion
+        #region Mobile Inputs
+        if(Input.touches.Length>0)
+        {
+            if(Input.touches[0].phase==TouchPhase.Began)
+            {
+                tap = true;
+                isDraging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if(Input.touches[0].phase==TouchPhase.Ended||Input.touches[0].phase==TouchPhase.Canceled)
+            {
+                isDraging = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        swipeDelta = Vector2.zero;
+        if(isDraging)
+        {
+            if (Input.touches.Length > 0)
+                swipeDelta = Input.touches[0].position - startTouch;
+            else if (Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+
+        if(swipeDelta.magnitude>pointMort)
+        {
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+
+            if(Mathf.Abs(x)>Mathf.Abs(y))
+            {
+                //left and right
+                if(x<0)
+                {
+                    swipeLeft = true;
+                }
+                else
+                {
+                    swipeRight = true;
+                    
+                }
+            }
+            else
+            {
+                //up and down
+                if(y<0)
+                {
+                    swipeDown = true;
+
+                }
+                else
+                {
+                    swipeUp = true;
+                }
+            }
+            Reset();
+        }
+    }
+    private void Reset()
+    {
+        startTouch =swipeDelta= Vector2.zero;
+        isDraging = false;
+    }
+
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+
+    public bool Tap { get { return tap; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeDown { get { return swipeDown; } }
+   
+}
