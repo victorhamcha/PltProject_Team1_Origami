f3dc914363eb39a62834df7280b5e2b6c7844431
Theo Driutti <theo.driutti57@gmail.com>
2020-11-12 15:26:49 +0100

    SoundManager

---
+++ Origami_PltProject_Team1/Assets/Scripts/TheoScripts/GameAssets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    private static GameAssets _i;
+    public static GameAssets i
+    {
+        get
+        {
+            if (_i = null) _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+
+            return _i;
+        }
+    }
+
+    public SoundAudioClip[] soundAudioClipArray;
+
+    [Serializable]
+    public class SoundAudioClip
+    {
+        public SoundManager.Sound sound;
+        public AudioClip clip;
+    }
+
+    public LoopAudioClip[] loopAudioClipArray;
+
+    [Serializable]
+    public class LoopAudioClip
+    {
+        public SoundManager.Loop loop;
+        public AudioClip clip;
+    }
+}
