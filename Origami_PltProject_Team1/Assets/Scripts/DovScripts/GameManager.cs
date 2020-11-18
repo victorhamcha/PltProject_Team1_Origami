034f6fc5bfe832a454e1c483a3a2ee01b606c258
Dov <dovbenzak2@hotmail.com>
2020-11-18 18:45:18 +0100

    [FEAT] Debut GameManager

---
+++ Origami_PltProject_Team1/Assets/Scripts/DovScripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    private static GameManager instance;
+
+    private GameManager() { }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameManager();
+            }
+            return instance;
+        }
+    }
+
+    public void Pause(bool pause)
+    {
+        if (pause)
+        {
+            //ce qui se passe en pause
+        }
+        else
+        {
+            //resume
+        }
+    }
+}
