c2d33f249e2dd1658c49a99ba4f218bd8a80f77e
Vincent <vincent_1999@hotmail.fr>
2020-11-27 11:52:03 +0100

    [FIX] Speed rotation model 3D

--- Origami_PltProject_Team1/Assets/Scripts/JulienScripts/ItemRotation.cs
+++ Origami_PltProject_Team1/Assets/Scripts/JulienScripts/ItemRotation.cs
@@ -4,73 +4,69 @@ using UnityEngine;
 
 public class ItemRotation : MonoBehaviour
 {
-	private Touch _touch;
+    private Touch _touch;
 
-	private Quaternion _rotationY;
-	private Quaternion _rotationX;
+    private Quaternion _rotationY;
 
-	[SerializeField] private float _multiplier = 0.3f;
-	[SerializeField] private float _autoRotationSpeed = 0.1f;
-	[SerializeField] private float _startRotationTimer = 0.1f;
-	 private float _timerAutoRotation = 0.0f;
-	[SerializeField] private float _smoothRotationDuration = 0.0f;
-	[SerializeField] private AnimationCurve curveRotation;
-	 private float _timerBackToY = 0.0f;
+    [SerializeField] private float _multiplier = 0.3f;
+    [SerializeField] private float _autoRotationSpeed = 0.1f;
+    [SerializeField] private float _startRotationTimer = 0.1f;
+    private float _timerAutoRotation = 0.0f;
+    [SerializeField] private float _smoothRotationDuration = 0.0f;
+    [SerializeField] private AnimationCurve curveRotation;
+    private float _timerBackToY = 0.0f;
 
-	private bool _hasReset = false;
+    private bool _hasReset = false;
 
-	private Quaternion _lastPos = Quaternion.identity;
-	private Quaternion _currentpos = Quaternion.identity;
+    private Quaternion _lastPos = Quaternion.identity;
+    private Quaternion _currentpos = Quaternion.identity;
 
-	void Update()
-	{
-		if(Input.touchCount > 0)
+    void Update()
+    {
+        if (Input.touchCount > 0)
         {
-			_touch = Input.GetTouch(0);
-			_timerBackToY = 0.0f;
-			_timerAutoRotation = _startRotationTimer;
+            _touch = Input.GetTouch(0);
+            _timerBackToY = 0.0f;
+            _timerAutoRotation = _startRotationTimer;
 
-			if(_touch.phase == TouchPhase.Began && _hasReset)
+            if (_touch.phase == TouchPhase.Began && _hasReset)
             {
-				_lastPos = transform.rotation;
+                _lastPos = transform.rotation;
             }
 
-			if (_touch.phase == TouchPhase.Moved)
-			{
-				_hasReset = false;
-				_rotationY = Quaternion.Euler(_touch.deltaPosition.y * _multiplier, -_touch.deltaPosition.x * _multiplier, 0f);
-				
-				transform.localRotation = _rotationY * transform.localRotation;
-			}
-
-			if(_touch.phase == TouchPhase.Ended)
+            if (_touch.phase == TouchPhase.Moved)
             {
-				_currentpos = transform.rotation;
+                _hasReset = false;
+                _rotationY = Quaternion.Euler(_touch.deltaPosition.y * _multiplier, -_touch.deltaPosition.x * _multiplier, 0f);
+
+                transform.localRotation = _rotationY * transform.localRotation;
+            }
+
+            if (_touch.phase == TouchPhase.Ended)
+            {
+                _currentpos = transform.rotation;
             }
 
         }
         else
         {
-			_timerAutoRotation -= Time.deltaTime;
+            _timerAutoRotation -= Time.deltaTime;
 
-			if(_timerAutoRotation < 0f)
+            if (_timerAutoRotation < 0f)
             {
-				if(_timerBackToY <= _smoothRotationDuration)
+                if (_timerBackToY <= _smoothRotationDuration)
                 {
-					transform.rotation = Quaternion.Lerp(_currentpos,_lastPos, curveRotation.Evaluate((_timerBackToY / _smoothRotationDuration)));
-					_timerBackToY += Time.deltaTime;
-					Debug.Log(curveRotation.Evaluate(_timerBackToY / _smoothRotationDuration));
-					
+                    transform.rotation = Quaternion.Lerp(_currentpos, _lastPos, curveRotation.Evaluate((_timerBackToY / _smoothRotationDuration)));
+                    _timerBackToY += Time.deltaTime;
                 }
-				else
+                else
                 {
-					_hasReset = true;
+                    _hasReset = true;
                 }
 
-
-				_rotationY = Quaternion.Euler(0f, _autoRotationSpeed, 0f);
-				transform.localRotation = _rotationY * transform.localRotation;
-			}
+                _rotationY = Quaternion.Euler(0f, _autoRotationSpeed * Time.deltaTime, 0f);
+                transform.localRotation = _rotationY * transform.localRotation;
+            }
         }
-	}
+    }
 }
