76c7ac1cbbd14220a624d3deb4e809fbbece32f5
Julien Dor <julien.dor3@gmail.com>
2020-11-30 17:54:27 +0100

    Zoom / Dezoom Origami with Post Process (Vignette)

---
+++ Origami_PltProject_Team1/Assets/Scripts/JulienScripts/ZoomVignette.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ZoomVignette : MonoBehaviour
+{
+    private Vignette _vignette = null;
+    [SerializeField] private Volume _volume;
+
+    [SerializeField] [Range(0, 100)] private float variablePli = 0.0f;
+    [SerializeField] private float _intensityFactor = 0.05f;
+    [SerializeField] private float _zoomFactor = 0.1f;
+    private float _intensityValue = 0.1f;
+
+    [SerializeField] private float _startOrthoSize = 0.0f;
+    [SerializeField] private float _endOrthoSize = 0.0f;
+    [SerializeField] private float _changedOrthoSize = 0.0f;
+    [SerializeField] private Camera _cam = null;
+
+    private float _timerDezoom = 0.0f;
+    public bool _testBool = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _volume = GetComponent<Volume>();
+        _volume.profile.TryGet<Vignette>(out _vignette);
+        _startOrthoSize = _cam.orthographicSize;
+        _changedOrthoSize = _startOrthoSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (_testBool)
+        {
+            VignetteZoom();
+        }
+        else
+        {
+            VignetteDezoom();
+        }
+
+        // intensité = (% du pli / 100) * facteur ([0;1]);
+        // orthographicSize -= facteurZoom;
+    }
+
+    private void VignetteZoom()
+    {
+        _intensityValue = (variablePli / 100) * _intensityFactor;
+        _vignette.intensity.value = _intensityValue;
+        _changedOrthoSize -= (variablePli / 100) * _zoomFactor;
+        _cam.orthographicSize = _changedOrthoSize;
+        _endOrthoSize = _changedOrthoSize;
+        _changedOrthoSize = _startOrthoSize;
+
+    }
+
+    private void VignetteDezoom()
+    {
+        _timerDezoom += Time.deltaTime;
+        _vignette.intensity.value = Mathf.Lerp(_intensityValue, 0, _timerDezoom);        
+        _cam.orthographicSize = Mathf.Lerp(_endOrthoSize, _startOrthoSize, _timerDezoom);
+    }
+}
