d6fe0b8b68e9f8510dd441e1124fbaf43effcb1d
Vincent <vincent_1999@hotmail.fr>
2020-12-03 17:09:55 +0100

    [FEAT] Switch Object script + Origami prefab

---
+++ Origami_PltProject_Team1/Assets/Scripts/VincentScripts/Origami/SwitchObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchObject : MonoBehaviour
+{
+    private int index = 0;
+    [SerializeField] private List<GameObject> listObjectToDisplayByOrder = new List<GameObject>();
+
+    private void Start()
+    {
+        if (listObjectToDisplayByOrder.Count > 0)
+            listObjectToDisplayByOrder[index].SetActive(true);
+    }
+
+    public void DisplayNextObject()
+    {
+        if (listObjectToDisplayByOrder.Count <= 0)
+            return;
+        listObjectToDisplayByOrder[index].SetActive(false);
+        if (index + 1 < listObjectToDisplayByOrder.Count)
+        {
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }
+        listObjectToDisplayByOrder[index].SetActive(true);
+    }
+
+    public void DisplayPreviousObject()
+    {
+        if (listObjectToDisplayByOrder.Count <= 0)
+            return;
+        listObjectToDisplayByOrder[index].SetActive(false);
+        if (index - 1 >= 0)
+        {
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }
+        listObjectToDisplayByOrder[index].SetActive(true);
+    }
+
+}
