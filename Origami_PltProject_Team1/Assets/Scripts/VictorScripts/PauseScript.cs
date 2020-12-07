11a4846129cc0f6e167971c2b110b3647676e108
Hamcha <victor.hamcha@gmail.com>
2020-12-07 03:09:39 +0100

    toute l'UI fini

---
+++ Origami_PltProject_Team1/Assets/Scripts/VictorScripts/PauseScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    bool paused;
+    // Start is called before the first frame update
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    public void Pause ()
+    {
+        paused = !paused;
+
+        if(paused)
+        {
+            Time.timeScale = 0;
+            Debug.Log("Paused");
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Debug.Log("Unpaused");
+        }
+    }
+}
