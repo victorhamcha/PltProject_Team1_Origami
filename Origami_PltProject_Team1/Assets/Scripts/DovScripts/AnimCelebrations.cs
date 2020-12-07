7ca931dd25edcf4f0a46e92813e018ab9afe912b
Vincent <vincent_1999@hotmail.fr>
2020-12-07 13:43:35 +0100

    [FEAT] Bandeau titre unroll for oiseau and Animations for celebration screen

---
+++ Origami_PltProject_Team1/Assets/Scripts/DovScripts/AnimCelebrations.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class AnimCelebrations : MonoBehaviour
+{
+    [SerializeField] private Animator _animator = null;
+    [SerializeField] private AnimationClip _animFadeOut = null;
+    [SerializeField] private VideoPlayer _videoPlayer = null;
+
+    private void Start()
+    {
+        _animator.speed = 0;
+    }
+
+    public void PlayAnimButtonFadeOut()
+   {
+        _animator.speed = 1;
+        _animator.Play(_animFadeOut.name);
+
+        //_videoPlayer.targetTexture.Release();
+        _videoPlayer.Play();
+   }
+
+    public float GetNormalizedTimeAnimator()
+    {
+        return _animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+}
