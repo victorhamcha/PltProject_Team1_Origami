a98cbf6dceee759d2047022eda7a16fa94d3a7b1
Julien Dor <julien.dor3@gmail.com>
2020-12-08 17:21:46 +0100

    Wind Done

---
+++ Origami_PltProject_Team1/Assets/Scripts/JulienScripts/WindManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class WindManager : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCam = null;
+    [SerializeField] private VideoClip _windClip = null;
+    public GameObject windGo = null;
+    private Vector3 _screenPoint;
+    private Vector3 _worldPos;
+    private float _randomSpeed = 0.0f;
+
+    void Start()
+    {
+        StartCoroutine("SetupWind");
+    }
+
+    IEnumerator SetupWind()
+    {
+
+        
+       
+        if (!GameManager.Instance.GetSwitchModePlayerOrigami()._isOnModeOrigami && !PauseScript.paused)
+        {
+            _randomSpeed = Random.Range(1.0f, 5.0f);
+            _screenPoint = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), _mainCam.nearClipPlane);
+            _worldPos = _mainCam.ScreenToWorldPoint(_screenPoint);
+            GameObject wind = Instantiate(windGo, _worldPos, Quaternion.identity);
+
+            yield return new WaitForSeconds((float)_windClip.length + _randomSpeed);
+            Destroy(wind);
+            StartCoroutine("SetupWind");
+        }
+    }
+}
