02b2019c82afc5676daf82c2a6fefd7b53ea02a5
Dov <dovbenzak2@hotmail.com>
2020-12-08 20:16:02 +0100

    [FEAT] Add vfx movement

--- Origami_PltProject_Team1/Assets/Scripts/DovScripts/Entity/TopDownController.cs
+++ Origami_PltProject_Team1/Assets/Scripts/DovScripts/Entity/TopDownController.cs
@@ -6,17 +6,29 @@ using Rewired;
 
 public class TopDownController : MonoBehaviour
 {
+    [SerializeField] private GameObject _moveParticle;
+    private GameObject _ob;
     public Entity entity;
     public LayerMask playerMask;
+    private float _timer = 0.2f;
+    private float duration = 0.2f;
 
     void Update()
     {
+        _timer -= Time.deltaTime;
         if (entity.moveModeOn)
         {
             ClickClickManager.Instance.RaycastClick(playerMask);
             if (ClickClickManager.Instance.isTouch && ClickClickManager.Instance.isTouchTarget)
             {
                 entity.MoveToDestination(ClickClickManager.Instance.hit.point);
+
+                if (_timer <= 0)
+                {
+                    Destroy(_ob);
+                    _ob = Instantiate(_moveParticle, ClickClickManager.Instance.hit.point, Quaternion.identity);
+                    _timer = duration;
+                }
             }
         }
 
