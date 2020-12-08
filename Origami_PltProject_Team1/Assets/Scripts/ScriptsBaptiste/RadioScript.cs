369368af105c90d2743c83d4fb183d8a57cbe4bf
Dov <dovbenzak2@hotmail.com>
2020-12-08 19:25:39 +0100

    [FEAT] RadioScript + prefab

--- Origami_PltProject_Team1/Assets/Scripts/ScriptsBaptiste/RadioScript.cs
+++ Origami_PltProject_Team1/Assets/Scripts/ScriptsBaptiste/RadioScript.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 
 public class RadioScript : MonoBehaviour
 {
-    [SerializeField]
-    private AudioSource RadioSource;
+    [SerializeField] private GameManager _gameManager = null;
+    [SerializeField] private GameObject _bulle = null;
 
-    [SerializeField]
-    private AudioClip[] RadioClips;
+    [SerializeField] private AudioSource RadioSource;
+    [SerializeField] private AudioClip[] RadioClips;
 
-    [SerializeField]
-    private GameObject MusicParticles;
-   
-    // Faire commencer la radio
+    [SerializeField] private GameObject MusicParticles;
+    private bool _isTrigger = false;
+    public LayerMask layerBubule;
 
+    private void Awake()
+    {
+        _gameManager = GameManager.Instance;
+    }
 
-    private void RadioOn()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !_isTrigger )
+        {
+            _isTrigger = true;
+            _bulle.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _bulle.SetActive(false);
+            _isTrigger = false;
+        }
+    }
+
+    private void ClickClickBubule()
     {
         int ClipIndex = Random.Range(0, RadioClips.Length);
         RadioSource.PlayOneShot(RadioClips[ClipIndex]);
         MusicParticles.gameObject.SetActive(true);
     }
+
+    private void Update()
+    {
+        ClickClickManager.Instance.RaycastClick(layerBubule);
+        if (ClickClickManager.Instance.isTouch && ClickClickManager.Instance.isTouchTarget && _isTrigger)
+        {
+            ClickClickBubule();
+        }
+    }
+
 }
