374972d4ddabfb511a595fe810247c9e66fd3cfb
Vincent <vincent_1999@hotmail.fr>
2020-12-08 16:16:58 +0100

    [FEAT] Mouse version

--- Origami_PltProject_Team1/Assets/Scripts/DovScripts/ClickClickManager.cs
+++ Origami_PltProject_Team1/Assets/Scripts/DovScripts/ClickClickManager.cs
@@ -9,6 +9,9 @@ public class ClickClickManager : MonoBehaviour
     [HideInInspector] public bool isTouch;
     [HideInInspector] public bool isTouchTarget;
     [HideInInspector] public TouchPhase touchPhase;
+    [HideInInspector] public bool onMobile = false;
+    public Vector3 touchDeltaPosition;
+    private Vector3 lastPos = Vector3.zero;
     private static ClickClickManager _instance;
 
     public static ClickClickManager Instance
@@ -39,11 +42,13 @@ public class ClickClickManager : MonoBehaviour
 
         if (Input.touchCount > 0)
         {
+            onMobile = true;
             isTouch = true;
             Touch touch = Input.GetTouch(0);
             touchPhase = touch.phase;
-
+            lastPos = touchPos;
             touchPos = new Vector3(touch.position.x, touch.position.y, 0);
+            touchDeltaPosition = touch.deltaPosition;
             ray = Camera.main.ScreenPointToRay(touchPos);
 
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask))
@@ -59,10 +64,26 @@ public class ClickClickManager : MonoBehaviour
         }
         else if (Input.GetMouseButton(0))
         {
+            onMobile = false;
             isTouch = true;
-
             touchPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
 
+            if (touchPos != lastPos)
+            {
+                touchDeltaPosition = new Vector3(touchPos.x - lastPos.x, touchPos.y - lastPos.y, touchPos.z - lastPos.z);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                touchPhase = TouchPhase.Began;
+                touchDeltaPosition = Vector3.zero;
+            }
+            else
+            {
+                touchPhase = TouchPhase.Moved;
+            }
+
+            lastPos = touchPos;
             ray = Camera.main.ScreenPointToRay(touchPos);
 
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask))
@@ -83,4 +104,5 @@ public class ClickClickManager : MonoBehaviour
             touchPhase = TouchPhase.Canceled;
         }
     }
+
 }
