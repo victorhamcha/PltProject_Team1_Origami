256a7297aec817d3b1f97208e7aa5a51dffcb3a0
Vincent <vincent_1999@hotmail.fr>
2020-12-09 20:09:42 +0100

    [FIX] For Build

--- Origami_PltProject_Team1/Assets/Scripts/DovScripts/GameManager/GameManager.cs
+++ Origami_PltProject_Team1/Assets/Scripts/DovScripts/GameManager/GameManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
