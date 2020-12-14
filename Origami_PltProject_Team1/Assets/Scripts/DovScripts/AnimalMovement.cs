b26e796bf52534beff217dc7927a32c00d2f86bd
Dov <dovbenzak2@hotmail.com>
2020-12-14 13:13:03 +0100

    [FIX] bubule tag + debut navmesh

---
+++ Origami_PltProject_Team1/Assets/Scripts/DovScripts/AnimalMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalMovement : MonoBehaviour
+{
+    private float _timer = 0f;
+    public Transform pathPoint;
+    private NavMeshAgent agent = null;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.destination = pathPoint.position;
+        //agent.Warp(pathPoint.position);
+        //agent.Resume();
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= 0.5f)
+        {
+            agent.destination = pathPoint.position;
+            _timer = 0;
+        }
+    }
+
+
+}
