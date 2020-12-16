74a2a9bdebb85f76bab8703a47903b182361bab0
Vincent <vincent_1999@hotmail.fr>
2020-12-16 09:54:18 +0100

    [FIX] Async scene and scale video intro and destroy canvas end video

---
+++ Origami_PltProject_Team1/Assets/DestroyObjectEndVIdeo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class DestroyObjectEndVIdeo : MonoBehaviour
+{
+    public bool isPlaying = false;
+    [SerializeField] private Canvas _cinematicCanvas = null;
+    [SerializeField] private VideoClip _cinematicClip = null;
+
+    private float timer = 0f;
+
+    void Update()
+    {
+        if (isPlaying)
+        {
+            timer += Time.deltaTime;
+            if (timer > (_cinematicClip.length - 8f))
+            {
+                Destroy(_cinematicCanvas.gameObject);
+            }
+        }
+        Debug.Log("Timer : " + timer);
+    }
+}
