6ed9339a8dafc13d1aded9fac2fe95166d01fb2d
Vincent <vincent_1999@hotmail.fr>
2020-12-19 16:36:39 +0100

    [FIX] LD SCENE / PREFABS + RESCALE ORIGAMI BY SCREEN WIDTH

---
+++ Origami_PltProject_Team1/Assets/RescaleBySizeScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescaleBySizeScreen : MonoBehaviour
+{
+    private Vector2 _screenSize = new Vector2(1080, 1920);
+    private Vector3 _initScale = new Vector3();
+
+    private void Awake()
+    {
+        _initScale = transform.localScale;
+        float facteurScreenSize = (Screen.width / _screenSize.x);
+        transform.localScale = new Vector3(facteurScreenSize * _initScale.x, facteurScreenSize * _initScale.y, facteurScreenSize * _initScale.z);
+    }
+}
